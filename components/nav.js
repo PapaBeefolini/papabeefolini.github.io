892dde42ee471e782433f927c54308b696dee8c9
Papa Brickolini <jasonrtraeder@gmail.com>
2023-08-20 17:25:54 -0400

    Add resume, tweak tools, fix navigation

--- components/nav.js
+++ components/nav.js
@@ -7,12 +7,11 @@ class Nav extends HTMLElement {
         this.innerHTML = `
             <nav>
                 <div class="row">
-                    <a href="index.html"><h7>Home</h7></a>
+                    <a href="/"><h7>Home</h7></a>
                     <div class="row index">
-                        <a href="index.html#about"><h7>About Me</h7></a>
-                        <a href="index.html#positions"><h7>Positions</h7></a>
-                        <a href="index.html#projects"><h7>Projects</h7></a>
-                        <a href="index.html#gallery"><h7>Gallery</h7></a>
+                        <a href="/#about"><h7>About Me</h7></a>
+                        <a href="/#projects"><h7>Projects</h7></a>
+                        <a href="/#resume"><h7>Resume</h7></a>
                     </div>
                     <a href="javascript:void(0);" class="icon" onclick="myFunction()">
                         <i class="fa fa-bars"></i>
